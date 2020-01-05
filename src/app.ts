3977cf5a997f1ed6a45f07b9a5af5f32a435d36b
cokia <hwymaster01@gmail.com>
2020-01-05 21:15:14 +0900

    add api docs

--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,11 @@ app.get("/", (req: express.Request, res: express.Response) =>{
 app.listen(3000,() => {
   console.log("✅Start app server✅")
 });
+
+app.get('/', function(req, res) {
+  res.send('Welcome to dimi-tranding-repo api server!');
+});
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
