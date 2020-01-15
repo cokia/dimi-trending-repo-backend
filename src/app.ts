5eb6abc4430a95aad0181c68a80fa0ef3f5aad74
cokia <hwymaster01@gmail.com>
2020-01-16 01:39:58 +0900

    db finish ✅

--- src/app.ts
+++ src/app.ts
@@ -4,54 +4,54 @@ import { addDBUser } from './db';
 import cors from 'cors';
 
 class App {
-  public application: Application;
+	public application: Application;
 
-  constructor() {
-    this.application = express();
-  }
+	constructor() {
+		this.application = express();
+	}
 }
 
 const app = new App().application;
 app.use(cors());
 app.all('/*', function(req, res, next) {
 
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	next();
 });
 
 app.listen(3001,() => {
-  console.log('✅ Start dimi-tranding-repo api server✅');
-  getAllRepo('cokia');
+	console.log('✅ Start dimi-tranding-repo api server✅');
+	getAllRepo('cokia');
 
 });
 
 app.get('/', function(req, res) {
-  res.status(200).send('Welcome to dimi-tranding-repo api server!');
+	res.status(200).send('Welcome to dimi-tranding-repo api server!');
 });
 
 app.get('/api/v1/get/rankedrepo', function(req,res) {
-  res.status(400).send({ error: 'is still develop..OTL i will dev ASAP :D' });
+	res.status(400).send({ error: 'is still develop..OTL i will dev ASAP :D' });
 });
 
 app.get('/api/v1/get/rankeduser', function(req, res) {
-  res.status(400).send({ error: 'is still develop..OTL i will dev ASAP :D' });
+	res.status(400).send({ error: 'is still develop..OTL i will dev ASAP :D' });
 
 });
 
 app.post('/api/v1/useradd', function(req, res) {
-  let name  = req.query.name;
-  console.log(name);
-  let department = req.query.department;
-  let year = req.query.year;
+	let name  = req.query.name;
+	console.log(name);
+	let department = req.query.department;
+	let year = req.query.year;
 
-  let githubid = req.query.githubid;
-  try {
-    addDBUser(name,department,year,githubid,'1');
-    res.status(200).send('success');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err);
-  }
-  getAllRepo(githubid);
+	let githubid = req.query.githubid;
+	try {
+		addDBUser(name,department,year,githubid,'1');
+		res.status(200).send('success');
+	} catch (err) {
+		console.error(err);
+		res.status(500).send(err);
+	}
+	getAllRepo(githubid);
 });
--- src/db.ts
+++ src/db.ts
@@ -4,109 +4,127 @@ const Schema = mongoose.Schema;
 
 mongoose.connection.on('error', console.error);
 mongoose.connection.once('open', function() {
-  console.log('✅ Connected to mongod server✅');
+	console.log('✅ Connected to mongod server✅');
 });
 
 mongoose.connect('mongodb://docker.cloudus.io/dimi-tranding-repo-test',{ useNewUrlParser: true,useUnifiedTopology: true });
 
 interface IRepo extends Document {
-  // rank: string;
-  username: string;
-  department: string;
-  year: string;
-  githubid: string;
-  reponame: string;
-  repourl: string;
-  description: string;
-  stargazer: string;
-  stargazer_count: string;
-  forkazger: string;
-  language: string;
+	// rank: string;
+	username: string;
+	department: string;
+	year: string;
+	githubid: string;
+	reponame: string;
+	repourl: string;
+	description: string;
+	stargazer: string;
+	stargazer_count: string;
+	forkazger: string;
+	language: string;
 }
 
 const repoSchema = new Schema({
-  // rank: {type: String, required: true},
-  username: { type: String, required: true },
-  department: { type: String, required: true },
-  year: { type: String, required: true },
-  githubid : { type: String, required: true },
-  reponame: { type: String, required: true },
-  repourl: { type: String, required: true },
-  description: { type: String, required: false },
-  stargazer: { type: Array, required: true },
-  stargazer_count: { type: String, required: true },
-  forkazger_count: { type: String, required: true },
-  language: { type: String, required: false }
+	// rank: {type: String, required: true},
+	username: { type: String, required: true },
+	department: { type: String, required: true },
+	year: { type: String, required: true },
+	githubid : { type: String, required: true },
+	reponame: { type: String, required: true },
+	repourl: { type: String, required: true },
+	description: { type: String, required: false },
+	stargazer: { type: Array, required: true },
+	stargazer_count: { type: String, required: true },
+	forkazger_count: { type: String, required: true },
+	language: { type: String, required: false }
 });
 
 const Repo = mongoose.model<IRepo>('repo', repoSchema);
 
 interface IUser extends Document {
-  name: string;
-  department: string;
-  year: string;
-  githubid: string;
+	name: string;
+	department: string;
+	year: string;
+	githubid: string;
 }
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  department: { type: String, required: true },
-  year: { type: String, required: true },
-  githubid: { type: String, required: true }
+	name: { type: String, required: true },
+	department: { type: String, required: true },
+	year: { type: String, required: true },
+	githubid: { type: String, required: true }
 });
 
 const User = mongoose.model<IUser>('user', userSchema);
 export async function addDBUser(name: string, department: string, year: string, githubid: string, starcount: string) {
-  let user = new User({
-    name: name,
-    department: department,
-    year: year,
-    githubid: githubid,
-    starcount: starcount
-  });
-  user.save();
+  const count =userdocsexist(githubid)
+  if (!count) {
+	let user = new User({ name,githubid,year,department,starcount });
+} else if(count) {
+  console.log('exist user' + name + githubid);
+  }
 }
 
 export async function addDBRepo(username: string, reponame: string, repourl: string, description: string,  stargazer: string[] | undefined,stargazer_count: string, forkazger_count: string, language: string | undefined) {
 //   try {
-  const Repo = mongoose.model('repo', repoSchema);
-  const userInfo = await callFromUserDB(username);
-  if (!userInfo) {
-    console.log('no user info. user add error');
-    return;
+	const Repo = mongoose.model('repo', repoSchema);
+	const userInfo = await callFromUserDB(username);
+	if (!userInfo) {
+	console.log('no user info. user add error');
+	return;
   }
-  if (!language) {
-    console.log('No language info');
-    language = 'no';
+	if (!language) {
+	console.log('No language info');
+	language = '';
   }
-  if (!description) {
-    console.log('no description');
+	if (!description) {
+	console.log('no description');
+	description = '';
   }
   const { department, year, githubid } = userInfo;
-
-  if (Repo.find()) {
-    let repo = new Repo({ username, department, year, githubid, reponame, repourl, description, stargazer, stargazer_count, forkazger_count, language });
-    repo.save();
-  } else if (Repo.find()) {
-    let before_id = Repo.find();
-    Repo.update(
-  { githubid,reponame },
-  { username,department,year,githubid,reponame,repourl,description,stargazer,stargazer_count,forkazger_count,language }, /* query */
-  { upsert: true });
+  const count =repodocsexist(repourl)
+	if (!count) {
+	let repo = new Repo({ username, department, year, githubid, reponame, repourl, description, stargazer, stargazer_count, forkazger_count, language });
+  repo.save();
+  } else if(count) {
+	Repo.update(
+	{ githubid,reponame },
+	{ $set: { description,stargazer,stargazer_count,forkazger_count,language } }, /* query */
+	{ multi: true });
   }
 }
 
+export async function repodocsexist(repourl:string){
+  Repo.countDocuments({repourl: repourl},function(err, count){
+    if(count>0){
+      return true;
+    }
+    else{
+      return false;
+    }
+}
+  )}
+export async function userdocsexist(githubid:string){
+  User.countDocuments({githubid: githubid}, function (count:number){ 
+    if(count>0){
+      return true;
+    }
+    else{
+      return false;
+    }
+}
+  )}
 export async function callFromUserDB(username: string) {
-  return User.findOne({ githubid: username });
+	return User.findOne({ githubid: username });
 }
 export async function repoDBRank() {
-  try {
-    Repo.find(function(repos) {
-  // console.log(repos);
-      return(repos);
-    });
+	try {
+	Repo.find(function(repos) {
+	// console.log(repos);
+	return(repos);
+  });
   } catch (error) {
-    console.log('Error!');
+	console.log('Error!');
   }
 }
 
